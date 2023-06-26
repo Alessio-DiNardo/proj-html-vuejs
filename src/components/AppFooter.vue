f3802a3cc5b4bafff91186d9793fe79efa9f3aea
Alessio-DiNardo <Alessiodinardo9@gmail.comm>
2023-06-26 16:42:57 +0200

    finish maxi structurre

--- src/components/AppFooter.vue
+++ src/components/AppFooter.vue
@@ -1,5 +1,22 @@
 <template>
-
+    <div class="container">
+        <p></p>
+        <section class="footer">
+                <div>
+                    <p>
+                        Copyright 2012-2023 | <span>Avada Website Builder</span> by <span>ThemFusion</span> | All Rights reserrved | Powererd by <span>WordPress</span>
+                    </p>
+                </div>
+                <div>
+                    <ul class="icons">
+                        <li><i class="fa-brands fa-square-facebook"></i></li>
+                        <li><i class="fa-brands fa-square-twitter"></i></li>
+                        <li><i class="fa-brands fa-square-instagram"></i></li>
+                        <li><i class="fa-brands fa-square-youtube"></i></li>
+                    </ul>
+                </div>
+            </section>
+    </div>
 </template>
 <script>
 export default {
--- src/components/AppMain.vue
+++ src/components/AppMain.vue
@@ -1,4 +1,24 @@
 <template>
+<section class="spam">
+    <div class="container">
+        <img src="" alt="">
+        <p>"We have worked with Avada Law for the past 15 years. As we have grown and evolved, what i have valued most about our relationship is that Avada Law know us and our business"</p>
+        <p>Nigel Rawlinson . Senior Partner </p>
+    </div>
+</section>
+<section class="background">
+    <div class="container-container-card">
+        <div class="card"></div>
+        <div class="card"></div>
+        <div class="card"></div>
+    </div>
+</section>
+<section class="work">
+    <h1></h1>
+    <div class="line"></div>
+    <p></p>
+    <button></button>
+</section>
 
 </template>
 <script>
@@ -9,4 +29,35 @@ export default {
 <style lang="scss" scoped>
     @use '../styles/partials/mixins' as*;
     @use '../styles/partials/variables' as*;
+
+    section.spam{
+        width: 100%;
+        background-color: blue;
+        height: 120px;
+        
+    }
+
+    section.background{
+        background-color: $sixColor;
+        width: 100%;
+        height: 400px;
+    }
+    div.container-container-card{
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        height: 500px;
+    }
+    div.card{
+        width: 375px;
+        height: 300px;
+        background-color: red;
+        border: 1px solid black;
+        margin-bottom: 4rem;
+    }
+    section.work{
+        width: 100%;
+        height: 300px;
+        background-color: blue;
+    }
 </style>
