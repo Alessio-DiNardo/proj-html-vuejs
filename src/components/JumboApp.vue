86f9379d04891db3e97d23203899df9c71737c5a
Alessio-DiNardo <Alessiodinardo9@gmail.comm>
2023-06-27 15:08:04 +0200

    add  jumbo

--- src/components/JumboApp.vue
+++ src/components/JumboApp.vue
@@ -1,6 +1,14 @@
 <template>
 <div class="background">
-
+    <section class="text">
+        <h1>
+            Lawyerrs with a global reputation
+        </h1>
+        <h3>Contantly challenging ourselves to delive moe to our clients</h3>
+        <button>
+            Let's get started
+        </button>
+    </section>
 </div>
 </template>
 <script>
@@ -14,7 +22,34 @@ export default {
 
     div.background{
         width: 100%;
-        height: 600px;
-        background-color: blue;
+        height: 460px;
+        background-image: url('../assets/img/video_law_home_preview.jpg');
+    }
+
+    section.text{
+        color: white;
+        text-align: center;
+        padding-top: 10rem;
+        
+        h1{
+            padding-bottom: .8rem;
+            font-size: 2.5rem;
+            text-transform: uppercase;
+        }
+
+        h3{
+            font-size: 1rem;
+            text-transform: uppercase;
+            padding-bottom: 3rem
+        }
+    }
+
+    button{
+        border: none;
+        background-color: $firstColor;
+        padding: 1.2rem 5rem 1.2rem 5rem;
+        color: white;
+        text-transform: uppercase;
+        font-size: 1rem;
     }
 </style>
